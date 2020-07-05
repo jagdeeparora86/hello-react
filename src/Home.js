23262e2511d5ca89cdcaea92b72abc9cb25f4e2f
Jagdeep Singh <jagdeep.singh@veritas.com>
2020-07-04 17:15:16 -0700

    adding the gid

--- src/Home.js
+++ src/Home.js
@@ -15,11 +15,7 @@ class Home extends Component {
       </div>
       <p className="lead">
         This simple app creates meetings, allows people to check
-        in, and picks random users to award giveaways. It's a
-        good example of a Single Page Application which includes
-        connection to a database and routing. It's a practical
-        way to learn <a href="https://reactjs.org/">React</a>{' '}
-        with <a href="https://firebase.google.com">Firebase</a>.
+        in.  It's a way to learn <a href="https://reactjs.org/">React</a>{' '}
       </p>
       {user == null  && (      
           <>
